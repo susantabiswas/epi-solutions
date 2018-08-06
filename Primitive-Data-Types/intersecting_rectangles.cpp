ad7a98fe961d88fe277cdc42180e9f4d66938d94
susantabiswas <susanta18@outlook.com>
2018-08-06 21:27:22 +0530

    Intersection of rectangles

---
+++ Primitive-Data-Types/intersecting_rectangles.cpp
@@ -0,0 +1,22 @@
+/*
+    Problem:
+        Given information about two axis parallel rectangles, find whether these two rectangles intersect or not.
+        If they intersect, then give information about the shared area.
+
+    Input Rectangle data:
+        Coordinate of the lower left end, height and width of the rectangle.
+
+    Output:
+        (interection?, lower end point of shared rectangular area, width, height)
+        if intersection is there: 1 else 0
+        Others: real nos. if intersection is there, else -1    
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
