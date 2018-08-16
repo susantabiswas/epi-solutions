faa39daea8927ad077b98e6527e9e4743086b153
susantabiswas <susanta18@outlook.com>
2018-08-16 21:54:57 +0530

    uniform number generator

---
+++ Primitive-Data-Types/uniform_random_no_generator.cpp
@@ -0,0 +1,21 @@
+/*
+    Problem:
+        Given an unbiased function that generates 0 or 1, write a function that can generate 
+        a random number between the given range [a,b].
+
+    Input:
+        Lower limit:a, Upper limit:b
+
+    Output:
+        A real number between [a,b]
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main(){
+    
+    return 0;
+}
