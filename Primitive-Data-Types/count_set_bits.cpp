18cbb0fc9af6dd23107094f1b75db49514d84f17
susantabiswas <susanta18@outlook.com>
2018-08-18 21:23:29 +0530

    count no. of set bits

--- Primitive-Data-Types/count_set_bits.cpp
+++ Primitive-Data-Types/count_set_bits.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 // for counting no. of set bits 
+// Tc: O(n), n=no. of bits in number representation
 short countSetBits(unsigned int x){
     short set_bits = 0;
     // traverse through the bits 
@@ -21,9 +22,24 @@ short countSetBits(unsigned int x){
     return set_bits;
 }
 
+// counts set bits
+// TC: O(s), s=no. of set bits
+// we can efficiently count the set bits by going through only the set bit
+// using the x & (x-1) expression we can clear the rightmost set bit
+short countSetBits2(unsigned int x){
+    int set_bits = 0;
+    while(x){
+        set_bits++;
+        // clear the rightmost bit
+        x = x & (x-1);
+    }
+
+    return set_bits;
+}
+
 int main(){
-    cout << countSetBits(0) << endl;
-    cout << countSetBits(1) << endl;
-    cout << countSetBits(7) << endl;
+    cout << countSetBits(0) << ", " << countSetBits2(0) << endl;
+    cout << countSetBits(1) << ", " << countSetBits2(1) << endl;
+    cout << countSetBits(7) << ", " << countSetBits2(7) << endl;
     return 0;
 }
