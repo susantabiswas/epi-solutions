c5eab4ae4b0e0310beb4d45dd157e81f74600a2f
susantabiswas <susanta18@outlook.com>
2018-08-19 22:34:11 +0530

    same weight closest number

--- Primitive-Data-Types/same_weight_number.cpp
+++ Primitive-Data-Types/same_weight_number.cpp
@@ -13,14 +13,33 @@
 
 #include<iostream>
 #include<vector>
+#include<bits/functexcept.h>
 using namespace std;
 
 // Finds the same weight number satisfying the constraints
+/* In order to have the number with same weight which is closest, we can say that
+    the we need to minimize 2^m - 2^n, where m>n and m and n are the indices that
+    gets swapped. And that difference is the difference between the original and 
+    new number. To minimize that we take m,n as right as possible and ensuring
+    that the two are not same also. Also nth position should be adjacent to minimize.  
+*/
 int sameWeightNumber(int x){
-
+    // no. of bits
+    int word_size = 32;
+    
+    for(int i = 0; i < word_size - 1; i++){
+        // if the adjacent bits are not same
+        if((x >> i)&1 != (x >> i+1)&1){
+            // flip the bits
+            x ^= (1 << i) | (1 << i+1);
+            return x;
+        }
+    }
+    // when all the bits of the number are same i.e, either 1 or 0
+    throw __throw_invalid_argument;
 }
 
 int main(){
-    cout << sameWeightNumber();
+    cout << sameWeightNumber(~0);
     return 0;
 }
