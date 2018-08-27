7a0504bed8086f32b0fe071690fb4350a512eb62
susantabiswas <susanta18@outlook.com>
2018-08-27 22:11:08 +0530

    run length encoding decoding

---
+++ Strings/run_length_encoding.cpp
@@ -0,0 +1,35 @@
+/*
+    Problem:
+        Run length encoding: Given a string consisiting of chars, we represent
+        each same successive chars by their number followed by that char.
+        Eg: aaabcddda
+            3a1b1c3d1a
+            aaa
+            3a
+        Write functions for both encoding a string and decoding a run length encoded string 
+    Input:
+        str: string either encoded/decoded
+
+    Output:
+        encoded string or decoded string
+         
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// does run length encoding of the string
+string encodeRunLength(const string& str){
+
+}
+
+// for decoding run length length encoded strings
+string decodeRunLength(const string& str){
+
+}
+
+int main(){
+    cout << runLengthEncoding() << endl;    
+    return 0;
+}
