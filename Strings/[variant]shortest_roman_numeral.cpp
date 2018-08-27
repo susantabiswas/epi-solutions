b5d0336fce511b33ae25bd299cd5f01f21193a80
susantabiswas <susanta18@outlook.com>
2018-08-27 22:10:58 +0530

    Int to roman number using min symbols

--- Strings/[variant]shortest_roman_numeral.cpp
+++ Strings/[variant]shortest_roman_numeral.cpp
@@ -9,7 +9,7 @@
     Output:
         shortest roman num: string
 
-    TC: O(n), n: no. of symbols
+    TC: O(n), n: int num
 */
 
 #include<iostream>
@@ -17,38 +17,45 @@
 #include<unordered_map>
 using namespace std;
 
+
+// Int to roman symbols mappings
+unordered_map<int, string> romanMappings = {{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, {10, "X"},
+                                            {40, "XL"}, {50, "L"}, {90, "XC"},
+                                            {100, "C"}, {400, "CD"}, {500, "D"},
+                                            {900, "CM"}, {1000, "M"}};
+// roman number values
+/* Possible Roman symbols:
+    I:1, V:5, X:10, L:50, C:100, D:500, M:1000
+    1. I before V and X: IV:4 and IX:9
+    2. X before L and C: XL:40 and XC:90
+    3. C before D and M: CD:400 and CM:900
+ */
+vector<int> romanVals = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
 // converts roman number to integer
 /*
-    The integer equivalent is the sum of the values of the symbols except for the
-    symbols where the smaller symbol preceedes the greater symbol, in that case the value is
-    (value greater sym - value smaller sym)
-    Eg: XIV: 10 + (5-1)
+    We divide the number by the largest roman value that is smaller than
+    the roman number.
 */
+string shortestRomanfromInt(int num){
+    string roman_num;
 
-int romanToInteger(const string& roman_num){
-    // resultant int
-    int result = 0;
+    if(num == 0)
+        return "";
 
-    // roman symbols mappings
-    unordered_map<char, int> romanMappings = {{'I',1}, {'V',5}, {'X',10}, {'L',50},
-                                                {'C',100}, {'D',500}, {'M',1000}};
-    // scan through the symbols
-    for(int i = 0; i < roman_num.size(); i++){
-        // see the next symbol
-        if(i+1 < roman_num.size() && roman_num[i+1] > roman_num[i]){
-            result += ((romanMappings[roman_num[i+1]]) - (romanMappings[roman_num[i]]));
-                // increment index since the next index has already been accounted for
-            ++i;
+    // start with the largest value
+    for(int i = romanVals.size() - 1; i >= 0 ; i--){
+        if(romanVals[i] <= num){
+            // print the sym the no. of times it can divide
+            for(int j = 0; j < num / romanVals[i]; j++)
+                roman_num += romanMappings[romanVals[i]];
 
+            return roman_num + shortestRomanfromInt(num % romanVals[i]);
         }
-        else
-            result += romanMappings[roman_num[i]];
     }
-    return result;
 }
 
 int main(){
-    cout << romanToInteger("XIV") << endl << romanToInteger("XIVII") << endl
-        << romanToInteger("VIII") << endl;
+   cout << shortestRomanfromInt(10) << endl << shortestRomanfromInt(59) << endl
+      << shortestRomanfromInt(900) << endl << shortestRomanfromInt(1300) << endl;
     return 0;
 }
