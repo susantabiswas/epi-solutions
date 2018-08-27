755e598cb3cad9a3e41e83d0d9c91c6f88c51237
susantabiswas <susantasky@gmail.com>
2018-08-27 17:32:18 +0800

    roman number to integer

---
+++ Strings/roman_numeral_to_int.cpp
@@ -0,0 +1,54 @@
+/*
+    Problem:
+        Given a valid number in roman numeral system, convert that to its int equivalent
+        I:1, V:5, X:10, L:50, C:100, D:500, M:1000
+
+    Input:
+        roman_num: string
+
+    Output:
+        Int equivalent
+
+    TC: O(n), n: no. of symbols
+*/
+
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+// converts roman number to integer
+/*
+    The integer equivalent is the sum of the values of the symbols except for the
+    symbols where the smaller symbol preceedes the greater symbol, in that case the value is
+    (value greater sym - value smaller sym)
+    Eg: XIV: 10 + (5-1)
+*/
+
+int romanToInteger(const string& roman_num){
+    // resultant int
+    int result = 0;
+
+    // roman symbols mappings
+    unordered_map<char, int> romanMappings = {{'I',1}, {'V',5}, {'X',10}, {'L',50},
+                                                {'C',100}, {'D',500}, {'M',1000}};
+    // scan through the symbols
+    for(int i = 0; i < roman_num.size(); i++){
+        // see the next symbol
+        if(i+1 < roman_num.size() && roman_num[i+1] > roman_num[i]){
+            result += ((romanMappings[roman_num[i+1]]) - (romanMappings[roman_num[i]]));
+                // increment index since the next index has already been accounted for
+            ++i;
+
+        }
+        else
+            result += romanMappings[roman_num[i]];
+    }
+    return result;
+}
+
+int main(){
+    cout << romanToInteger("XIV") << endl << romanToInteger("XIVII") << endl
+        << romanToInteger("VIII") << endl;
+    return 0;
+}
