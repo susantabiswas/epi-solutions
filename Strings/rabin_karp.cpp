e204cfbb81fd0e38c7b9c883de61760cab67f10e
susantabiswas <susantasky@gmail.com>
2018-09-04 09:45:33 +0530

    rabin karp algo

--- Strings/rabin_karp.cpp
+++ Strings/rabin_karp.cpp
@@ -4,19 +4,20 @@
 
     Input:
         patt: string pattern
-        text: string text   
+        text: string text
 
     Output:
         matched indices: vector of indices
-    
+
     TC: Avg case: O(m + n)
         Worst case: O(mn)
         m: length of pattern
-        n; legth of text 
+        n; legth of text
 */
 
 #include<iostream>
 #include<vector>
+#include<cmath>
 #include<string>
 using namespace std;
 
@@ -24,12 +25,14 @@ using namespace std;
     The idea behind Rabin Karp is we use a hash function to compute the
     hash value of the pattern and text window. When the hash value of the
     window matches that of the pattern, then we check if the pattern is actually
-    present or not. 
+    present or not.
+
+    For hash function we use the decimal no. formed by the chars mod some prime number
 */
 vector<int> rabinKarp(string& text, string& patt){
 
     // base for the 26 alphabatical chars
-    int base = 26; 
+    int base = 10;
     // base ^ length of (pattern)
     long int power_patt = 0;
     // length of the pattern
@@ -37,9 +40,9 @@ vector<int> rabinKarp(string& text, string& patt){
     // length of txt
     int N = text.length();
     // hash value of pattern
-    int hash_patt = 0;
+    long int hash_patt = 0;
     // hash value of text
-    int hash_text = 0;
+    long int hash_text = 0;
     // number for doing the mod
     int q = 101;
     // for storing the matched indices
@@ -47,29 +50,36 @@ vector<int> rabinKarp(string& text, string& patt){
 
     // find the hash value of the pattern and the first window of text
     for(int i = 0; i < M; i++){
-        power_patt *= base;
-        hash_patt = (hash_patt * base + patt[i]) % q;
-        hash_text = (hash_text * base + text[i]) % q;
+        power_patt = power_patt ? base * power_patt:1;
+        hash_patt = hash_patt * base + patt[i];
+        hash_text = hash_text * base + text[i];
     }
+    hash_patt %= q;
+    hash_text %= q;
 
     // traverse through the text using the window
-    for(int i = 1; i <= N - M; i++){
+    for(int i = 0; i <= N - M; i++){
         // check if the hash values match
         if(hash_patt == hash_text){
-            // check if it actually matches
-            if(text.compare(i, M, patt) == 0)
+            // check if it actually matches text
+            if(text.compare(i, M, patt) == 0){
                 indices.emplace_back(i);
+            }
         }
 
         // adjust the hash value for the new window
-        hash_patt = hash_patt - (power_patt * text[i-1] % q);
-        hash_patt = hash_patt * base + text[i] % q;
+        hash_text = hash_text - (power_patt * text[i]) % q ;
+        hash_text = (hash_text * base + text[i+M]) ;
+
     }
 
-
+    return indices;
 }
 
 int main(){
-    
+    string text = "abaacab", patt = "a";
+    vector<int> indices = rabinKarp(text, patt);
+    for(int i = 0; i< indices.size(); i++)
+        cout << indices[i] << " ";
     return 0;
 }
