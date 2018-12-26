11c75be0376ecbd53b42410452bff6b87d493114
Susanta Biswas <susantasky@gmail.com>
2018-12-26 16:45:55 +0530

    Create word_sequence.cpp

---
+++ Graph/word_sequence.cpp
@@ -0,0 +1,23 @@
+/*
+	Given a dictionary of words, starting string 's' and target string 't', check if there is 
+	a sequence of words which starts with 's' and ends with 't'. The condition is that each
+	adjacent word should differ only in one position and all the words in the sequence should 
+	be of same length. Return the smallest sequence length if there are many, else return -1.
+	
+	Input: Dictionary of string, s, t
+	Output: Return the smallest sequence length if there are many, else return -1
+*/
+#include <iostream>
+#include<unordered_set>
+using namespace std;
+
+// structure for each node
+struct node{
+	string word;
+	int dist = 0;
+};
+
+int main() {
+	
+	return 0;
+}
