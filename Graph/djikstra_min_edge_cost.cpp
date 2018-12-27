43b7a97d9070ad113249abfb637784c0e5b6ebd6
Susanta Biswas <susantasky@gmail.com>
2018-12-27 17:57:34 +0530

    Create djikstra_min_edge_cost.cpp

---
+++ Graph/djikstra_min_edge_cost.cpp
@@ -0,0 +1,18 @@
+/*
+	Given a weighted graph that can be either cyclic/acyclic and directed/undirected. 
+	Given a starting and target node, find the min cost path with min number of edges.
+	
+	Input : Graph g
+	Output: min cost 
+	
+	Solution:
+		We can use Djikstra's shortest path algorithm with some modifications.
+		For comparing the cost we can include no. of edges also.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
