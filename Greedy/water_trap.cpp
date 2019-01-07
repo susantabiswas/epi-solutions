d8ec8663bfe987667c270cac9ab351836cae78bc
Susanta Biswas <susantasky@gmail.com>
2019-01-07 17:22:20 +0530

    Create water_trap.cpp

---
+++ Greedy/water_trap.cpp
@@ -0,0 +1,20 @@
+/*
+	Given an array representing height coordinates and assume their index position
+	as the x-coordinate. Find the pair of heights which makes the largest volume for
+	trapping water inside.
+	
+	Input: vector of numbers
+	Output: pair of heights
+	
+	Solution:
+			
+	TC: O()
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	return 0;
+}
