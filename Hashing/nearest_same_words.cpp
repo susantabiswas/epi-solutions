2c7fe2b569b66905aa84e4ceac7aec2168bf15a9
Susanta Biswas <susantasky@gmail.com>
2019-01-29 15:50:41 +0530

    Distance between nearest same words

---
+++ Hashing/nearest_same_words.cpp
@@ -0,0 +1,45 @@
+/*
+	Given an array of string, find the closest distance between the same words in the array
+
+	Input: array of string
+	Output: int
+
+	Solution:
+		Use hash table: key: string, value: index
+		For each entry in array:
+			if present in hash table:
+
+
+		TC: O(n)
+		SC: O(m), m: no. of distinct words
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <limits>
+using namespace std;
+
+// finds the closest distance between same words
+int findClosesWordsDistance(const vector<string>& arr) {
+	int closest_dist = numeric_limits<int>::max();
+
+	// stores the last known index of words
+	unordered_map<string, int> last_word_idx;
+
+	for(int i = 0; i < arr.size(); i++) {
+		// if current word was there previously, compute the distance
+		if(last_word_idx.find(arr[i]) != last_word_idx.end()) {
+			closest_dist = min(closest_dist, i - last_word_idx[arr[i]]);
+		}
+		// update/insert the current word
+		last_word_idx[arr[i]] = i;
+	}
+	return closest_dist;
+}
+
+int main() {
+	vector<string> arr = {"I", "apple", "banana", "was", "apple", "orange", "apple", "banana"};	
+	cout << findClosesWordsDistance(arr);
+	return 0;
+}
