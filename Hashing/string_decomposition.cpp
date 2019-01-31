00c5dee72eeab41f76f44d7d688ba61e117e0ec6
Susanta Biswas <susantasky@gmail.com>
2019-01-31 22:10:34 +0530

    concatenated words search in string

--- Hashing/string_decomposition.cpp
+++ Hashing/string_decomposition.cpp
@@ -13,8 +13,8 @@
 			characters and check if it is a word or not, then we continue for the remaining words by taking the
 			next 'm' characters. If all the words gets finished, then it is possible else return false
 
-		TC: O(n)
-		SC: O(m), 
+		TC: O(Nnm), N: no. of chars in sentence, n: no. of words, m: length of each word
+		SC: O(n) 
 */
 
 #include <iostream>
@@ -25,7 +25,36 @@
 #include <unordered_set>
 using namespace std;
 
-// checks if 
+bool doesSentenceContainWords(string& sentence, int& start, 
+				unordered_map<string, int>& word_freq, vector<string>& words) {
+	
+	// to keep track of word repeatitions
+	unordered_map<string, int> freq;
+	int word_len = words.front().size();
+	
+	// we take word_len no. of chars from sentence and check if that word is any amongst
+	// the given words
+	for(int i = 0; i < words.size(); i++) {
+		// extract word from sentence
+		string curr_word = sentence.substr(start + i * word_len, word_len);
+
+		// check if the word is there or not
+		auto it = word_freq.find(curr_word);
+
+		// if the word doesn't match means concated string not possible
+		if(it == word_freq.end()) 
+			return false;
+
+		++freq[curr_word];
+		// if a valid word appears more than required no. of times, that
+		// means some other valid word won't be able to come, so false
+		if(freq[curr_word] > word_freq[curr_word]) {
+			return false;
+		}
+	}
+	return true;
+}
+
 // checks if the given sentence contains the concatenation of words in any order
 // returns the starting index if so
 vector<int> stringDecompositionPossible(string& sentence, vector<string>& words) {
@@ -43,15 +72,14 @@ vector<int> stringDecompositionPossible(string& sentence, vector<string>& words)
 	for(int i = 0; i + word_length * num_words < sentence.size(); i++) {
 		// check if sentence starting 'i' contains the words or not
 		if(doesSentenceContainWords(sentence, i, word_freq, words))
-			result.emplace(i);
+			result.emplace_back(i);
 	}
-
-	return start;
+	return result;
 }
 
 int main() {
-	string sentence = "";
-	vector<string> words = {};
+	string sentence = "amanaplanacanal";
+	vector<string> words = {"can", "apl", "ana"};
 
 	vector<int> res = stringDecompositionPossible(sentence, words);
 	for(const auto& r: res) 
