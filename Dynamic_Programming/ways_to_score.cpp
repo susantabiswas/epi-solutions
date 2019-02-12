d04c2626ff80dffa070db36d3ab8a67f03ae0b57
Susanta Biswas <susantasky@gmail.com>
2019-02-12 15:10:21 +0530

    ways to achieve a score

--- Dynamic_Programming/ways_to_score.cpp
+++ Dynamic_Programming/ways_to_score.cpp
@@ -1,80 +1,74 @@
-/*	
-	Given an int score and a list of scores that can be scored by 
-    different moves. Find the total number of ways in which the score 
-    can be achieved. Possible scores for moves are 2, 3 and 7.
+/*
+    Given a int score and a scores for different moves/plays. Find the no. of ways in which that
+    score can be acheived.
 
-	Input: int, int array
+	Input: int, int
 	Output: int
-
+	
 	Solution:
-	    For a given score, we can either include the current move score or
-        ignore it.
-        If the current move score is greater than the final score, we try
-        the smaller scores.
-        TC: O(n)
+        Every similar to climbing stairs problem
+
+        TC: O(sn), s: score and n: no. of play scores
+
 */
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int waysToScoreTabulationHelper(int score, vector<int>& play_scores, 
-                vector<vector<int>>& cache) {
-    // if the score has been reached
+/*int waysToScoreTabulationHelper(int score, vector<int>& play_scores, 
+                vector<int>& cache) {
+    // when the score is reached
     if(score == 0)
         return 1;
     
-    // if the cached value is there
-    if(cache[score] != 0)
-        return cache[score];
-
-    for(int i = 0; i < play_scores.size() && play_scores[i] < score; i++) {
-        cache[score] += waysToScoreTabulationHelper(score - play_scores[i],
-                                                play_scores, cache);
+    // when the result is not cached, compute
+    if(cache[score] == 0) {
+        // try all play scores which can help achieve current score
+        for(int i = 0; i < play_scores.size() && score - play_scores[i] >= 0; i++) {
+            cache[score] += waysToScoreTabulationHelper(score - play_scores[i], play_scores, cache);
+        }
     }
-
     return cache[score];
 }
 
 int waysToScoreTabulation(int score, vector<int> play_scores) {
     // each ith entry stores the no. of ways in which that can be
     // acheived 
-    vector<int< cache(score + 1, 0);
+    vector<int> cache(score + 1, 0);
     return waysToScoreTabulationHelper(score, play_scores, cache);
-}
+}*/
 
-// helper function for the memoization functionality
-int waysToScoreMemoizationHelper(int score, int n, vector<int>& play_scores, 
-                            vector<vector<int>>& cache) {
-    // when the score has been reached
-    if(score == 0)
-        return 1;
-    
-    // if the current play score is larger, then try the next one
-    if(play_scores[n - 1] > score)
-        return cache[score][n-1] = waysToScoreMemoizationHelper(score, n-1, play_scores, cache);
-    
-    if(cache[score][n-1] == 0) {
-        // try all possible play scores till n
-        for(int i = 0, i < n; i++)
-            cache[score][n-1] += waysToScoreMemoizationHelper(score - play_scores[i], i + 1, play_scores, cache);
-    }
-    return cache[score][n-1];
-}
 
-// using memoization
-// finds the number of ways in which a score can be acheived
-int waysToScoreMemoization(int score, const vector<int>& play_scores) {
-    // each entry 'i', 'j' stores the number of ways in which
-    // that score can be acheived when play_scores till 'i' are allowed.
-    vector<vector<int>> cache(play_scores.size(), vector<int>(scores + 1, 0));
-
-    return waysToScoreMemoizationHelper(score, play_scores.size(), play_scores, cache);
+// finds the no. of ways a score can be acheived
+int waysToScore(int final_score, const vector<int>& play_scores) {
+	// each entry (i, j) stores the no. of ways in which score 'j' can
+	// be acheived if playscores till 'i' in play scores vector are allowed
+	vector<vector<int>> score_cmb(play_scores.size(), vector<int>(final_score + 1, 0));
+	
+	// we start from bottom and go upto the top 
+	for(int i = 0; i < play_scores.size(); i++) {
+		// score of 0 can be acheived in 1 way by every play score
+		score_cmb[i][0] = 1;
+		
+		// find the ways for building 'j' score 
+		for(int j = 1; j <= final_score; j++) {
+			// for getting current score, we can either
+			// include the current play score or 
+			// exclude the current play score
+			int exclude_current_score = i >= 1 ? score_cmb[i - 1][j] : 0;
+			int include_current_score = play_scores[i] <= j ? score_cmb[i][j - play_scores[i]] : 0;
+			
+			score_cmb[i][j] = include_current_score + exclude_current_score;
+		}
+	}
+	return score_cmb[play_scores.size() - 1][final_score];
 }
 
 int main() {
-    const vector<int> play_scores = {};
-    int score = ;
+    const vector<int> play_scores = {1,2,3};
+    int score = 5;
     cout << waysToScore(score, play_scores);
+    
     return 0;
 }
