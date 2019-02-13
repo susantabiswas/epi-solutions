41ea2df11a4c93ad3369390d2cf8ffb15796870b
Susanta Biswas <susantasky@gmail.com>
2019-02-13 17:10:36 +0530

    Create min_sum_path_triangle.cpp

---
+++ Dynamic_Programming/min_sum_path_triangle.cpp
@@ -0,0 +1,16 @@
+/*
+    Given array of increasing size such that when placed acc. to their size they form a traingle.
+    Find the min sum path starting from the top to the bottom array.
+
+    Solution:
+        
+        TC:
+        SC:
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
