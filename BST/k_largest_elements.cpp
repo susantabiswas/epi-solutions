e83127d13c16ab4e2a929edbb1d50b5c08aae862
Susanta Biswas <susantasky@gmail.com>
2019-02-21 10:55:43 +0530

    find K largest elements in BST

--- BST/k_largest_elements.cpp
+++ BST/k_largest_elements.cpp
@@ -2,9 +2,10 @@
     Given a binary tree and a value K, find the K largest elements.
     
     Solution:
-
-        TC: 
-        SC: 
+        We start the traversal with right, root and then left approach. This way we will get to the rightmost,
+        then we fallback to the next largest. We do this till k elements have been added.
+        TC: O(h + k), since we go to the deepest leaf on right, then ascend upwards k times
+        SC: O(h)
 */
 #include <iostream>
 #include <memory>
@@ -21,9 +22,32 @@ struct Node {
     Node(T data): data(data), left(nullptr), right(nullptr) {}
 };
 
-// finds the k largest elements 
-vector<Node<int>*> findKLargestElements(unique_ptr<Node<int>>& root, int k) {
+void findKLargestElementsHelper(unique_ptr<Node<int>>& root, int& k, 
+                                        vector<int>& result) {
+    if(!root)
+        return;
 
+    if(k > 0) {
+	    /*
+	        We will first hit the rightmost node which is the largest, then the next one will come once
+	        we recurse back and go to the next successor.Like inorder successor gives ascending sequence, 
+	        same way we are just doign inorder from reverse side so we get descending sequence.
+	    */
+	    findKLargestElementsHelper(root->right, k, result);
+	    if(k > 0) {
+	    	--k;
+		    result.emplace_back(root->data);
+		}
+	    findKLargestElementsHelper(root->left, k, result);
+    }
+}
+
+// finds the k largest elements 
+vector<int> findKLargestElements(unique_ptr<Node<int>>& root, int k) {
+    // for storing the k largest elements
+    vector<int> result;
+    findKLargestElementsHelper(root, k, result);
+    return result;
 }
 
 // inorder traversal
@@ -44,7 +68,7 @@ int main() {
                /  \   /  \
               1    3  7   9
                      /
-                     6
+                    6
     */
     unique_ptr<Node<int>> root = make_unique<Node<int>>(5);
     
@@ -60,7 +84,9 @@ int main() {
     root->right->right = make_unique<Node<int>>(9);
 
     inOrderTraversal(root.get());
-    
-    auto result = findKLargestElements(root, 3);
+    cout << endl;
+    auto result = findKLargestElements(root, 2);
+    for(const auto& a: result)
+        cout << a << " ";
     return 0;
 }
