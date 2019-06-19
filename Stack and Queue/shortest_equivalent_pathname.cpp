bd514dd69dfb122b0359b3f59e411a888d1a4562
Susanta <susantasky@gmail.com>
2019-06-19 22:05:00 +0530

    Shortest file path

--- Stack and Queue/shortest_equivalent_pathname.cpp
+++ Stack and Queue/shortest_equivalent_pathname.cpp
@@ -10,7 +10,7 @@
 		keep it. It might mean a relative path.
 		Note: if '..' appears after '/' then it is not possible to go before the root '/', so it won't be allowed
 		TC: O(n), n: no. of elements
-		SC: O(n) 
+		SC: O(n)
 */
 
 #include <iostream>
@@ -22,9 +22,9 @@ using namespace std;
 
 // finds the shortest equivalt path for a given input path
 string shortestEquivalentPath(const string& dir_path) {
-	if(dir_path.empty())
+	if (dir_path.empty())
 		throw length_error("Empty path");
-		
+
 	// string stream
 	stringstream ss(dir_path);
 	string token;
@@ -34,22 +34,22 @@ string shortestEquivalentPath(const string& dir_path) {
 	deque<string> path;
 
 	// root case: if the path starts from root then '/' shouldn't be ignored
-	if(dir_path.front() == '/')
+	if (dir_path.front() == '/')
 		path.emplace_back("/");
 
-	while(getline(ss, token, delimiter)) {
+	while (getline(ss, token, delimiter)) {
 		// if token is other than "." and ".." and not empty add
-		if(token != "" && token != "." && token != "..") {
+		if (token != "" && token != "." && token != "..") {
 			path.emplace_back(token);
 		}
-		else if(token == "..") {
+		else if (token == "..") {
 			// if there is nothing before this or again ".."
 			// then just add it
-			if(path.empty() || path.back() == "..")
+			if (path.empty() || path.back() == "..")
 				path.emplace_back(token);
 			else {
 				// if last token was root "/", then we can't go up to parent
-				if(path.back() == "/")
+				if (path.back() == "/")
 					throw invalid_argument("Wrong path\n");
 				else {	// if there is a parent dir then remove that
 					path.pop_back();
@@ -58,9 +58,10 @@ string shortestEquivalentPath(const string& dir_path) {
 		}
 	}
 
-	if(path.empty())
+
+	if (path.empty())
 		return "";
-		
+	
 	// now add "/" between each term, ensuring that if the path starts with
 	string shortest_path;
 	deque<string>::iterator it = path.begin();
@@ -68,12 +69,12 @@ string shortestEquivalentPath(const string& dir_path) {
 	shortest_path = *it;
 	++it;	//second term
 	// if first term was '/' then we dont add another '/'
-	if(shortest_path == "/"){
+	if (shortest_path == "/" && it != path.end()) {
 		shortest_path += *it;
 		++it;
 	}
-	
-	for(; it != path.end(); it++) {
+
+	for (; it != path.end(); it++) {
 		shortest_path += "/" + *it;
 	}
 
@@ -81,8 +82,8 @@ string shortestEquivalentPath(const string& dir_path) {
 }
 
 int main() {
-	string dir_path = "./../";
+	string dir_path = "/.";
 	cout << shortestEquivalentPath(dir_path);
 	return 0;
 }
- 
+
