fe5175152446075e5927873999d8df456e15c4a4
Susanta <susantasky@gmail.com>
2019-12-09 23:00:15 +0530

    multiply two numbers using bit operators

--- Primitive-Data-Types/multiply_numbers.cpp
+++ Primitive-Data-Types/multiply_numbers.cpp
@@ -11,24 +11,49 @@
 */
 
 #include<iostream>
-#include<vector>
+#include<bitset>
 using namespace std;
 
-// Bitwise addition of two numbers
-unsigned int addBitwise(unsigned int a, unsigned int b){
-
+// adds two numbers using bit operators
+unsigned Add(unsigned sum, unsigned y) {
+    unsigned new_sum = 0, carry_in = 0, carry_out = 0, k = 1;
+    unsigned temp_sum = sum, temp_y = y;
+    
+    while(temp_sum || temp_y) {
+        // get the bits at kth position
+        unsigned sum_k = sum & k, y_k = y & k;
+        // check if a carry bit will be formed or not
+        carry_out = (sum_k & y_k) | (sum_k & carry_in) | (y_k & carry_in);
+        // set the bit at kth position
+        new_sum = new_sum | (carry_in ^ sum_k ^ y_k);
+        // update carry
+        carry_in = carry_out << 1;
+        k <<= 1, temp_sum >>= 1, temp_y >>= 1; 
+    }
+    return new_sum | carry_in;
 }
 
-// Multiplies two numbers using only bitwise operators
-/*   
-    We use normal multiplication method, where we mutiply the multiplicand 
-    by the mutliplier bit by bit.
-*/
-unsigned int mutiplyBitwise(unsigned int x, unsigned int y){
-
+// multiplies two numbers using bit operators only
+unsigned Multiply(unsigned x, unsigned y) {
+    unsigned sum = 0;
+    // since this is for binary numbers, we have only 1
+    // or 0. For multiplication with 1, we get the same 
+    // number otherwise 0
+    // Here y is multiplied by x
+    while(x) {
+        if(x & 1)
+            sum = Add(sum, y);
+        // since y is added to the total and each time it is
+        // shifted by 1 step in every level
+        y = y << 1;
+        x = x >> 1;
+    }
+    return sum;
 }
 
 int main(){
-    
+    unsigned x = 13, y = 9;
+    unsigned prod = Multiply(x, y);
+    cout << prod;
     return 0;
 }
